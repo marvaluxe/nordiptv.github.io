105969ab8310caf7c4bc685660f1b7a8f09c4179
Marvaluxe Design <93430393+marvaluxe@users.noreply.github.com>
2023-12-10 17:09:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
+
+ 
+//const faqs = document.querySelectorAll(".fq");
+ 
+//faqs.forEach((fq)=>{
+ // fq.addEventListener("click" , ()=> {
+//fq.classList.toggle("active");
+
+ // })
+//}
+//);
+
+const faqs = document.querySelectorAll(".fq");
+let activeFaq = null;
+
+faqs.forEach((fq) => {
+  fq.addEventListener("click", () => {
+    if (activeFaq && activeFaq !== fq) {
+      activeFaq.classList.remove("active");
+    }
+    fq.classList.toggle("active");
+    activeFaq = fq.classList.contains("active") ? fq : null;
+  });
+});
+
+
+
+
+
+
+
 const header = document.querySelector("header");
 const hamburgerBtn = document.querySelector("#hamburger-btn");
 const closeMenuBtn = document.querySelector("#close-menu-btn");
@@ -34,3 +82,7 @@ jQuery(document).ready(function () {
     });
     /*MODIFICATION END*/
 }); 
+
+
+
+
